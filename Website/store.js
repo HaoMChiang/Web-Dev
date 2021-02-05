bb1f6b6dc261a5ac316504fe6758f76da224bca7
Hao Chiang <haomingchiang@gmail.com>
2021-02-04 16:55:33 -0800

    add store.js

---
+++ Website/store.js
@@ -0,0 +1,35 @@
+if(document.readyState == 'loading') {
+    document.addEventListener('DOMContentLoaded', ready);
+} else {
+    ready();
+}
+
+function ready() {
+    var removeCartItemBtn = document.getElementsByClassName('btn-danger');
+    console.log(removeCartItemBtn);
+    for(var i = 0; i < removeCartItemBtn.length; i++) {
+        var button = removeCartItemBtn[i];
+        button.addEventListener('click', removeCartItem);
+    }
+}
+
+function removeCartItem(event) {
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+}
+
+function updateCartTotal() {
+    var cartItemContainer = document.getElementsByClassName('cart-items')[0];
+    var cartRows = cartItemContainer.getElementsByClassName('cart-row');
+    var total = 0;
+    for(var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i];
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0];
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0];
+        var price = parseFloat(priceElement.innerText.replace('$',''));
+        var quantity = quantityElement.value;
+        total += price * quantity;
+    }
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total;
+}
